93ee2501ff1ffdfe090fc12488091a1e40d2ab73
shashank <main.shashank@gmail.com>
2013-02-09 06:02:00 +0530

    Linked In Oauth some nonsense

---
+++ scripts/php/common/linkedin/ouathclient.php
@@ -0,0 +1,23 @@
+<?php
+ 
+define("CONSUMER_KEY", "fgj883d6l6hs");
+define("CONSUMER_SECRET", "bgGlOuMtbAwAykAg");
+ 
+$oauth = new OAuth(CONSUMER_KEY, CONSUMER_SECRET);
+
+try {
+	$request_token_response = $oauth->getRequestToken('https://api.linkedin.com/uas/oauth/requestToken');
+ 
+	if($request_token_response === FALSE) {
+	  throw new Exception("Failed fetching request token, response was: " . $oauth->getLastResponse());
+	} else {
+	  $request_token = $request_token_response;
+	  $oauth_token = $request_token['oauth_token'];
+	  $url = "https://api.linkedin.com/uas/oauth/authorize?oauth_token=$oauth_token";
+	  header("Location: $url");
+	}	
+} catch (Exception $e) {
+	echo "$e->getmessage()";
+}
+
+?>
