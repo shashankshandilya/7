52b9620db31fe3a253b0240ef10c2d353b54dfd9
shashank <main.shashank@gmail.com>
2013-02-09 07:28:00 +0530

    Changes

--- scripts/php/common/linkedin/auth.php
+++ scripts/php/common/linkedin/auth.php
@@ -1,10 +1,7 @@
 <?php
     session_start();
 
-    $config['base_url']             =   'http://dev2.zovi.com/7/scripts/php/common/linkedin/authclient.php';
-    $config['callback_url']         =   'http://dev2.zovi.com/7/scripts/php/common/linkedin/callback.php';
-    $config['linkedin_access']      =   'fgj883d6l6hs';
-    $config['linkedin_secret']      =   'bgGlOuMtbAwAykAg';
+    include_once "conf.php";
 
     include_once "linkedin.php";
 
---
+++ scripts/php/common/linkedin/conf.php
@@ -0,0 +1,6 @@
+<?php
+	$config['base_url']             =   'http://dev2.zovi.com/7/scripts/php/common/linkedin/authclient.php';
+  $config['callback_url']         =   'http://dev2.zovi.com/7/scripts/php/common/linkedin/callback.php';
+  $config['linkedin_access']      =   'fgj883d6l6hs';
+  $config['linkedin_secret']      =   'bgGlOuMtbAwAykAg';
+?>
