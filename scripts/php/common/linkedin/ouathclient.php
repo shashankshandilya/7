c18932c2f86e04397c421e212e25b1dfb307bd36
shashank <main.shashank@gmail.com>
2013-02-09 06:36:56 +0530

    code in

--- scripts/php/common/linkedin/ouathclient.php
+++ scripts/php/common/linkedin/ouathclient.php
@@ -20,4 +20,22 @@ try {
 	echo "$e->getmessage()";
 }
 
+$oauth->setToken($request_token['oauth_token'], $request_token['oauth_token_secret']);
+
+$access_token_url = 'https://api.linkedin.com/uas/oauth/accessToken';
+$access_token_response = $oauth->getAccessToken($access_token_url, "", $oauth_verifier);
+
+if($access_token_response === FALSE) {
+  throw new Exception("Failed fetching request token, response was: " . $oauth->getLastResponse());
+} else {
+  $access_token = $access_token_response;
+}
+
+print "Access Token:\n";
+printf("   - oauth_token        = %s\n", $access_token['oauth_token']);
+printf("    - oauth_token_secret = %s\n", $access_token['oauth_token_secret']);
+print "\n";
+print "You may now access protected resources using the access tokens above.\n";
+print "\n";
+
 ?>
